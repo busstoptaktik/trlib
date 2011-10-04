defea01b7b735b51b942f743744496155187c433
Simon Lyngby Kokkendorff <10113573+silyko@users.noreply.github.com>
2011-10-04 11:59:58 +0200

    Improved c# bindings - great fun actually :-)

--- .NET/Interface.cs
+++ .NET/Interface.cs
@@ -1,16 +1,40 @@
+/*
+ * Copyright (c) 2011, National Survey and Cadastre, Denmark
+ * (Kort- og Matrikelstyrelsen), kms@kms.dk
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ */
+
+/* 
+* Interface to the TrLib transformation library.
+* Modify library name below 
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
-
 namespace Kmstrlib.NET
 {
-	public static unsafe class Interface
+	public static class Interface
 	{
-		#region Interface to KMSTRLIB.DLL
+		#region Interface to TrLib.DLL
 		/// <summary>
 		/// KMSTRLIB return values:
 		/// </summary>
+		public const string TRLIB="TrLib.dll"; //should really be defined at compile time....
+		
 		public enum KMSTR_Error
 		{
 			KMSTR_OK = 0,  // No Error
@@ -22,30 +46,66 @@ namespace Kmstrlib.NET
 
 		// Import the relevant functions from the TrLib.dll 
 		// It is assumed that this dll (and dependent dll's) are in the
-		// same folder as this dll (or in a folder on the system search path)
+		// same folder as this __file__ (or in a folder on the system search path)
 
-		[DllImport("TrLib.dll")]
+		[DllImport(TRLIB)]
 		public static extern int InitLibrary(string folder);
 
-		[DllImport("TrLib.dll")]
+		[DllImport(TRLIB)]
 		public static extern void TerminateLibrary();
 
-		[DllImport("TrLib.dll")]
-		public static extern void* tropen(string mlb1,string mlb2);
+		[DllImport(TRLIB)]
+		unsafe public static extern void* tropen(string mlb1,string mlb2);
 
-        [DllImport("TrLib.dll")]
-        public static extern void trclose( void* tr);
+		[DllImport(TRLIB)]
+		unsafe public static extern void trclose( void* tr);
 
-		[DllImport("TrLib.dll")]
-		public static extern KMSTR_Error tr(
+		[DllImport(TRLIB)]
+		unsafe public static extern KMSTR_Error tr(
 			
 			void* TR,
-			double* XIn,
-			double* YIn,
-			double* ZIn,
+			double* X,
+			double* Y,
+			double* Z,
 			int npoints);
-
-
+		
+		
+		[DllImport(TRLIB)]
+		unsafe public static extern KMSTR_Error Transform(
+		
+			string mlb1,
+			string mlb2,
+			double* X,
+			double* Y,
+			double* Z,
+			int npoints);
+		
+		[DllImport(TRLIB)]
+		unsafe public static extern void GetTRVersion(byte* buf, int buf_length);
+		
+		unsafe public static string GetVersion()
+		{
+			byte[] buf= new byte[128];
+			fixed (byte* bufp=buf){
+			GetTRVersion(bufp,buf.Length);}
+			return System.Text.ASCIIEncoding.ASCII.GetString(buf).Trim().Replace("\0",""); //should go right as long as version is more less 'ascii' encoded....
+		}
+		
+                unsafe public static KMSTR_Error Transform(
+			
+			string mlb1, 
+			string mlb2, 
+			double[] X, 
+			double[] Y, 
+			double[] Z)
+		{
+			KMSTR_Error  error;
+			if ((X.Length!=Y.Length)||((Z!=null) && (Z.Length!=X.Length))){
+				throw new ArgumentException("Sizes of input arrays must agree!");}
+			fixed (double* x=X,y=Y,z=Z){
+				error=Transform(mlb1,mlb2,x,y,z,X.Length);}
+			return error;
+		}
 		/// <summary>
 		/// String representation of error messages
 		/// </summary>
--- .NET/TestProgram.cs
+++ .NET/TestProgram.cs
@@ -4,24 +4,39 @@ using System.Text;
 
 namespace Kmstrlib.NET
 {
-	unsafe class TestProgram
+	class TestProgram
 	{
 		static void Main(string[] args)
 		{
 			Interface.KMSTR_Error  error;
-            int err;
-            void* tr;
-			err = Interface.InitLibrary("C:\\Geoids\\");
-            System.Console.WriteLine("Init: {0}\n", err);
-            double X = 400000;
-			double Y = 6500000;
-			double Z = 0;
-            System.Console.WriteLine("in:  X: {0,8:f}  Y: {1,8:f} Z: {2,4:f}\n", X, Y, Z);
-            tr=Interface.tropen("utm32Eed50","utm32Ewgs84");
-			error =  Interface.tr(tr, &X, &Y, &Z, 1);
-            if (error != Interface.KMSTR_Error.KMSTR_OK) throw new Exception(Interface.GetKMSErrorMessage(error));
-            System.Console.WriteLine("out: X: {0,8:f}  Y: {1,8:f} Z: {2,4:f} ret: {3,0:d}\n", X, Y, Z,error);
-            Interface.trclose(tr);
+			int i,err;
+			if (args.Length>0){
+				System.Console.WriteLine("Initialising library.");
+				err = Interface.InitLibrary(args[0]);
+				System.Console.WriteLine("Init: {0}\n", err);}
+			else{
+				System.Console.WriteLine("Specify geoid library as first input parameter!");
+				return;}
+			System.Console.WriteLine("Loading functions from {0}",Interface.TRLIB);
+			string version=Interface.GetVersion();
+			System.Console.WriteLine("This is TrLib. v. {0}\n",version);
+			double[] X =new double[] {512200.1,512499.1};
+			double[] Y =new double[] {6143200.1,6143300.1};
+			double[] Z =new double[] {100,200};
+			for (i=0; i<X.Length; i++){
+				System.Console.WriteLine("in:  X: {0,8:f4}  Y: {1,8:f4} Z: {2,4:f4}", X[i], Y[i], Z[i]);}
+			System.Console.WriteLine("Transformation from utm32Eed50 to utm32Ewgs84:");
+			error=Interface.Transform("\nutm32Eed50","utm32Ewgs84\n",X,Y,Z);
+			if (error != Interface.KMSTR_Error.KMSTR_OK) 
+				throw new Exception(Interface.GetKMSErrorMessage(error));
+			for (i=0; i<X.Length; i++){
+				System.Console.WriteLine("out: X: {0,8:f4}  Y: {1,8:f4} Z: {2,4:f4} ret: {3:d}", X[i], Y[i], Z[i],error);}
+			System.Console.WriteLine("Transformation from utm32_wgs84 to utm32_ed50:");
+			error=Interface.Transform("\nutm32_wgs84","utm32_ed50\n",X,Y,null);
+			if (error != Interface.KMSTR_Error.KMSTR_OK) 
+				throw new Exception(Interface.GetKMSErrorMessage(error));
+			for (i=0; i<X.Length; i++){
+				System.Console.WriteLine("out: X: {0,8:f4}  Y: {1,8:f4} Z: {2,4:f4} ret: {3:d}", X[i], Y[i], Z[i],error);}
 		}
 	}
 }
